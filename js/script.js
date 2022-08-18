b17a05285ada5ca996f62fc8d12d7163f8f8f379
Romashka <rroma13579@gmail.com>
2022-08-18 14:07:01 +0200

    tenth commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 var date_creation = new Date('January 01, 2022 00:00:00')
 var month = date_creation.getMonth()
 var year = date_creation.getFullYear()
@@ -43,7 +42,8 @@ m_1 = month + 1
 var d = date
 var w = weekdays
 
-console.log(date_real)
+var buttoncount = [];
+var d1 = date
 
 function Button_Range() {
     for (m_r; m_r > m_1 - 1; m_r--) {
@@ -52,15 +52,19 @@ function Button_Range() {
     for (m; m < month_real; m++) {
         if (m == 2 && year_real % 4 == 0) {
             d -= 29
+            d1 +=29
         }
         else if (m == 2) {
             d -= 28
+            d1 +=28
         }
         else if (m == 4 || m == 6 || m == 9 || m == 11) {
             d -= 30
+            d1 +=30
         }
         else {
             d -= 31
+            d1 +=31
         }
     }
     if (w != 7) {
@@ -73,10 +77,35 @@ function Button_Range() {
     else if (date_real_help == date_real + 1) {
     }
     else if (date_real_help != date_real + 1) {
-        k -= date_real_help
+        d -= date_real_help
     }
-    for (d; d <= date_real; d++) {
-        buttons.innerHTML += "<button class='button'></button>"
+    for (d; d <= date_real; d++){
+        d1--
+        buttoncount.push(200)
+        buttons.innerHTML += "<button class='button' id="+d1+" onclick='Color("+d1+")' onpointerover='Button_Help("+d1+") onpointerout='Button_Delete("+d1+")'></button>"
     }
     date_real_help = date_real
 }
+function Color(d1){
+    var button_id = document.getElementById(""+d1+"")
+    rgb2 = buttoncount[d1]
+    button_id.style.background = "rgb(255, "+rgb2+", 255)"
+    if(buttoncount[d1] != 40){
+        buttoncount[d1] -= 40
+    }
+    else{}
+}
+// function Button_Help(d1){
+//     var d1_1 = document.getElementById(""+d1+"")
+//     d1_1.innerHTML += "<div id='button-help'>"
+//     var button_help = document.getElementById("button-help")
+//     button_help.style.width = "10px"
+//     button_help.style.height = "10px"
+//     button_help.style.background = "white"
+// }
+// function Button_Delete(d1){
+//     var d1_1 = document.getElementById(""+d1+"")
+//     d1_1.innerHTML += "<div id='button-help'>"
+//     var button_help = document.getElementById("button-help")
+//     button_help.remove()
+// }
